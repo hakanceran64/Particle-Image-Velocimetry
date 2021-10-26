4827e3c5c4d6d774b8f3e6149920d1c05376fbcf
Hakan Ceran <hakanceran64@gmail.com>
2021-10-26 15:53:36 +0300

    ilk test tamamlandı

--- localPIV.py
+++ localPIV.py
@@ -55,9 +55,9 @@ for i in range(0, 99, 1):
 
 # while False:    
     ## Processing
-    winsize    = 32   # pixels, interrogation window size in frame A
-    searchsize = 42   # pixels, search area size in frame B
-    overlap    = 17   # pixels, 50% overlap
+    winsize    = 25   # pixels, interrogation window size in frame A
+    searchsize = 36   # pixels, search area size in frame B
+    overlap    = 16   # pixels, 50% overlap
     dt         = 0.02 # sec, time interval between the two frames
     u0, v0, sig2noise = pyprocess.extended_search_area_piv(
         gray[0].astype(np.int32),
@@ -89,7 +89,7 @@ for i in range(0, 99, 1):
         'exp1_001.txt',
         ax=ax,
         scaling_factor=100,
-        scale=50, # scale defines here the arrow length
+        scale=60, # scale defines here the arrow length
         width=0.0025, # width is the thickness of the arrow
         on_img=True, # overlay on the image
         image_name=frame_a[i],
